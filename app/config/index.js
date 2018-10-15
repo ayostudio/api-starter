2cf9e0c7a66d431871f25312c7f3b55dffdc6dc2
Joseph Smith <joe@twocards.co>
2018-10-15 03:56:32 +0100

    hidden and updated sendgrid keys

--- app/config/index.js
+++ app/config/index.js
@@ -2,9 +2,7 @@ module.exports = {
   env: process.env.NODE_ENV || 'development',
   mongodbUri: process.env.MONGODB_URI || 'mongodb://localhost:27017/node-api',
   secret: process.env.SECRET || 'node-api-secret',
-  sendgridKey:
-    process.env.SENDGRID_KEY
-    || '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',
+  sendgridKey: process.env.SENDGRID_KEY,
   port: process.env.PORT || '4000',
   adminKey: process.env.ADMIN_KEY || 'adminKey',
 };
